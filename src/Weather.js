b50490e061df163b4f4ad06b66ee259b7ed7c3a9
Josie <sweetjosieme@gmail.com>
2022-09-22 18:15:56 -0700

    improved search

--- src/Weather.js
+++ src/Weather.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
 import "./App.js";
-import formatedDate from "./formatedDate";
+
+import WeatherInfo from "./WeatherInfo";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
-  function handleResponse(response) {
-    console.log(response.data);
+  const [city, setCity] = useState(props.defaultCity);
 
+  function handleResponse(response) {
     setWeatherData({
       ready: true,
       temperature: response.data.main.temp,
@@ -16,60 +17,52 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       city: response.data.name,
       description: response.data.weather[0].description,
-      date: "wednesday 8:27",
+      date: new Date(response.data.dt * 1000),
     });
+  }
+
+  function search() {
+    const apiKey = "22389e50fe25f5e989f8feefb616bbbf";
+    let apiUrl =
+      "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial";
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+  function handleCity(event) {
+    setCity(event.target.value);
+    search();
   }
 
   if (weatherData.ready) {
     return (
-      <div className="container">
-        <div className="weather shadow">
-          <form>
-            <div className="row">
-              <div className="col-9">
-                <input
-                  type="search"
-                  placeholder="City"
-                  className="form-input"
-                  autoFocus="on"
-                />
-                <input
-                  type="submit"
-                  value="search"
-                  className=" col-3 btn btn-primary"
-                />
-              </div>
-            </div>
-          </form>
-          <h1>{weatherData.city}</h1>
-          <ul>
-            <li>
-              <formatedDate date={weatherData.date} />
-            </li>
-            <li className="text-captalize">{weatherData.description}</li>
-          </ul>
+      <div className="weather shadow">
+        <form onSubmit={handleSubmit}>
           <div className="row">
-            <div className="col-6">
-              cloudy logo {Math.round(weatherData.temperature)}° F
-            </div>
-            <div className="col-6">
-              <ul>
-                <li>precipitation:{}</li>
-                <li>wind:{weatherData.wind}Km/h</li>
-                <li>humidity:{}</li>
-              </ul>
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="City"
+                className="form-input"
+                autoFocus="on"
+                onChange={handleCity}
+              />
+              <input
+                type="submit"
+                value="search"
+                className=" col-3 btn btn-primary"
+              />
             </div>
           </div>
-        </div>
+        </form>
+        <WeatherInfo data={props.weatherData} />
       </div>
     );
   } else {
-    const apiKey = "22389e50fe25f5e989f8feefb616bbbf";
-    let apiUrl =
-      "https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=imperial";
-
-    axios.get(apiUrl).then(handleResponse);
-
+    search();
     return "Loading..";
   }
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,28 @@
+import React from "react";
+import formatedDate from "./formatedDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1>{weatherData.city}</h1>
+      <ul>
+        <li>
+          <formatedDate date={props.weatherData.date} />
+        </li>
+        <li className="text-captalize">{props.weatherData.description}</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          cloudy logo {Math.round(props.weatherData.temperature)}° F
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>precipitation:{}</li>
+            <li>wind:{props.weatherData.wind}Km/h</li>
+            <li>humidity:{}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}

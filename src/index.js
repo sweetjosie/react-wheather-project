c81ef76bb240ae20513bbc594dfceff639065b41
Josie <sweetjosieme@gmail.com>
2022-09-24 03:49:15 -0700

    reinstalling react dependencies

--- src/index.js
+++ src/index.js
@@ -14,4 +14,6 @@ root.render(
     <Weather />
   </React.StrictMode>
 );
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
 reportWebVitals();
